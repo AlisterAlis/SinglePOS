d6e84dae4f3b22e07312ff004c6f17748a08be3e
AlisterAlis <107392816+AlisterAlis@users.noreply.github.com>
2022-06-13 13:02:12 +0200

    Update server.js
    Updated to static server js

--- server.js
+++ server.js
@@ -1,24 +1,56 @@
 import http from 'http';
+import url from 'url';
+import path from 'path';
 import fs from 'fs';
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    fs.createReadStream('index.html').pipe(res)
-  })
+    let port = process.argv[2] || 8888,
+
+    mimeTypes = {
+      "html": "text/html",
+      "jpeg": "image/jpeg",
+      "jpg": "image/jpeg",
+      "png": "image/png",
+      "svg": "image/svg+xml",
+      "json": "application/json",
+      "js": "text/javascript",
+      "css": "text/css"
+    };
+ 
+http.createServer(function(request, response) {
+ 
+  var uri = url.parse(request.url).pathname, 
+      filename = path.join(process.cwd(), uri);
   
-  server.listen(process.env.PORT || 3000)
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+ 
+    if (fs.statSync(filename).isDirectory()) 
+      filename += '/index.html';
+ 
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+      
+      var mimeType = mimeTypes[filename.split('.').pop()];
+      
+      if (!mimeType) {
+        mimeType = 'text/plain';
+      }
+      
+      response.writeHead(200, { "Content-Type": mimeType });
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
 
-/*
-const PORT=8080; 
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
-*/
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
